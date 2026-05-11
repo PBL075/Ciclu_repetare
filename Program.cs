3582eff6560df73cc5d7cd5fac48ca134e2d9882
PBL075 <pavelgorea75@gmail.com>
2026-05-11 16:08:04 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ciclu
+{
+    class Program
+    {
+        static void Main()
+        {
+            int [] number = new int [4];
+            number[0] = 1;
+            number[1] = 3;
+            number[2] = 5;
+            number[3] = 7;
+            
+
+            for (int i = 0 ; i < number.Length ; i++)
+            {
+                Console.WriteLine(" Number " + number[i]);
+            }
+            
+            
+        }
+    }
+}
